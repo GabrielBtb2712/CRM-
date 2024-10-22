b90258f6a5f6d4ac67a0a92d2e63697f6b6f074d
Dragon <gabriel2712bocanegrat@gmail.com>
2024-10-22 15:39:39 -0600

    Autentificacion creada

--- DJANGOAPPI/urls.py
+++ DJANGOAPPI/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from api.home.home_view import home_views
-from api.login.login_view import login_view
+from api.login.login_view import (login_view,register_view,forget_passsword)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', login_view, name='login'),
+    path('register/', register_view, name='register'),
+    path('forget_password/', forget_passsword, name='forget_password'),
     path('', home_views, name='index')
     
     
--- api/login/login_view.py
+++ api/login/login_view.py
@@ -1,5 +1,48 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 def login_view(request):
-    template_name = 'login.html'  
+    template_name = 'login.html'
+    if request.method == 'POST':
+        # Obtén el correo electrónico del formulario (nombre del campo 'username' en el formulario)
+        email = request.POST['username']  
+        password = request.POST['password']
+
+        try:
+            # Busca el usuario en la base de datos usando el correo
+            user = User.objects.get(email=email)
+            # Autentica al usuario con su username y contraseña
+            user = authenticate(request, username=user.username, password=password)
+
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Inicio de sesión exitoso.')
+                return redirect('index')
+            else:
+                messages.error(request, 'Contraseña incorrecta.')
+        except User.DoesNotExist:
+            messages.error(request, 'No existe un usuario con ese correo electrónico.')
+    
     return render(request, template_name)
+
+# Registrarse 
+
+def register_view(request):
+    template_name = 'register.html'  
+    return render(request, template_name)
+
+# Forget password ?
+
+def forget_passsword(request):
+    template_name = 'forget_password.html'  
+    return render(request, template_name)
+
+def log_out(request):
+    template_name =''
+
+
+
+
